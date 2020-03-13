ed751bef91ee171b429df8b3c15ce725d27c4d6d
trupci <andreea.trupcea@ing.com>
2020-03-14 01:26:20 +0200

    Api for static pages.

--- api/Decisions.Api/Startup.cs
+++ api/Decisions.Api/Startup.cs
@@ -1,9 +1,16 @@
+using System.Net;
+using System.Text;
 using Decisions.Api.ApiInfrastructure;
+using Decisions.Api.Models;
+using Decisions.Api.Repositories;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
 
 namespace DecisionsApi
 {
@@ -22,6 +29,9 @@ namespace DecisionsApi
             services.AddControllers();
 
             services.RegisterSwagger();
+            if (Configuration.GetValue<StorageType>("StorageType") == StorageType.FileSystem) {
+                services.AddTransient<IStaticPageRepository, FileSystemRepository>();
+            }
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,6 +40,26 @@ namespace DecisionsApi
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler(appError =>
+                {
+                    appError.Run(async context => {
+                        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                        context.Response.ContentType = "application/json";
+
+                        var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+                        if (contextFeature != null) {
+                            var errorResponse = JsonConvert.SerializeObject(new {
+                                StatusCode = context.Response.StatusCode,
+                                Message = $"Internal Server Error. {contextFeature.Error.Message}"
+                            });
+                            context.Response.ContentLength = errorResponse.Length;
+                            await context.Response.WriteAsync(errorResponse, Encoding.UTF8);
+                        }
+                    });
+                });
             }
 
             app.UseHttpsRedirection();
