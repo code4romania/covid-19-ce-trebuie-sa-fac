4e5cc26aee0cba1b8ef48b964355c8b9d6a5b41b
adascaliteiradu <adascalitei.radu@live.com>
2020-03-14 18:08:45 +0200

    chore: update global styling, removed custom elements, added mobile friendly css for the header, added node-sass

--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -1,34 +1,35 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import logo_small from "../images/logo_small.svg";
-import c4r_logo from "../images/c4r_logo.svg";
-import gov_logo from "../images/gov_logo.svg";
+import logo_small from "../../images/logo_small.svg";
+import c4r_logo from "../../images/c4r_logo.svg";
+import gov_logo from "../../images/gov_logo.svg";
+import "./Header.scss";
 
 const Header = () => {
   return (
     <header className="App-header">
-      <top-header>
-        <app-logo>
+      <div className="top-header">
+        <div className="app-logo">
           <Link to="/">
             <img alt="" src={logo_small}></img>
           </Link>
-        </app-logo>
+        </div>
         <nav>
           <Link to="/despre">Despre proiect</Link>
         </nav>
-      </top-header>
-      <bottom-header>
-        <app-credits>
+      </div>
+      <div className="bottom-header">
+        <div className="app-credits">
           <span>Un proiect dezvoltat de</span>
-          <a href="https://code4.ro/">
+          <a href="https://code4.ro/" target="_blank" rel="noopener noreferrer">
             <img alt="" src={c4r_logo}></img>
           </a>
           <span>În parteneriat cu </span>
-          <a href="https://www.gov.ro/">
+          <a href="https://www.gov.ro/" target="_blank" rel="noopener noreferrer">
             <img alt="" src={gov_logo}></img>
           </a>
-        </app-credits>
-      </bottom-header>
+        </div>
+      </div>
     </header>
   );
 };
