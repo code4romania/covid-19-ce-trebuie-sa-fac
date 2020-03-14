22df68922ccb280a59a8da136a498abc82c5b6db
Dan Lepadatu <lepadatudan@gmail.com>
2020-03-14 11:06:51 +0100

    refactor: change single to double quotes

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
 const About = () => {
-    return (
-        <div className="App-content">
-            <h1>Despre</h1>
-        </div>
-    );
+  return (
+    <div className="App-content">
+      <h1>Despre</h1>
+    </div>
+  );
 };
 
 export default About;
