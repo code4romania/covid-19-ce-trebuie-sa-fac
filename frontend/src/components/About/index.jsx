f1ba24bf38897a258830de28930595c83f6ce7fa
adascaliteiradu <adascalitei.radu@live.com>
2020-03-14 19:50:02 +0200

    chore: extract useful tools components

--- frontend/src/components/About/index.jsx
+++ frontend/src/components/About/index.jsx
@@ -1,14 +1,9 @@
 import React from "react";
 import covid_all from "../../images/covid_all.svg";
 import logo_large from "../../images/logo_large.svg";
-import {
-  FaFacebook,
-  FaLinkedin,
-  FaInstagram,
-  FaTwitter,
-  FaSearch
-} from "react-icons/fa";
+import { FaFacebook, FaLinkedin, FaInstagram, FaTwitter } from "react-icons/fa";
 import "./About.scss";
+import UsefulTools from "../UsefulTools";
 
 const About = () => {
   return (
@@ -89,59 +84,7 @@ const About = () => {
           </p>
         </section>
         <section className="about-section-useful-instruments">
-          <div className="section-title">
-            <img src={covid_all} alt=""></img>
-            <h1>Instrumente utile</h1>
-          </div>
-          <div className="useful-instruments">
-            <section>
-              <div className="search-bar">
-                <input placeholder="caută informații aici"></input>
-                <FaSearch size="18px"></FaSearch>
-              </div>
-              <div className="info-card info-card-news">
-                <div className="card-title">
-                  Instalează-ți add-on-ul de depistat știrile false
-                </div>
-              </div>
-              <div className="info-card info-card-news">
-                <div className="card-title">
-                  Stiri oficiale si informații la zi
-                </div>
-                <div className="card-content">
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Elit, duis pretium.
-                  </p>
-                  <button>Cele mai noi informații oficiale</button>
-                </div>
-              </div>
-            </section>
-            <section>
-              <div className="info-card info-card-help">
-                <div className="card-title">Vrei să ajuți. Intră aici</div>
-                <div className="card-content">
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Elit, duis pretium.
-                  </p>
-                  <button>Centrul de sprijin</button>
-                </div>
-              </div>
-            </section>
-            <section>
-              <div className="info-card info-card-data">
-                <div className="card-title">Date în timp real</div>
-                <div className="card-content">
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Elit, duis pretium.
-                  </p>
-                  <button>Vezi situația curentă </button>
-                </div>
-              </div>
-            </section>
-          </div>
+          <UsefulTools></UsefulTools>
         </section>
       </div>
     </div>
---
+++ frontend/src/components/UsefulTools/index.jsx
@@ -0,0 +1,66 @@
+import React, { Component } from "react";
+import { FaSearch } from "react-icons/fa";
+import covid_all from "../../images/covid_all.svg";
+import "./UsefulTools.scss";
+
+export default class UsefulTools extends Component {
+  render() {
+    return (
+      <div>
+        <div className="section-title">
+          <img src={covid_all} alt=""></img>
+          <h1>Instrumente utile</h1>
+        </div>
+        <div className="useful-tools">
+          <section>
+            <div className="search-bar">
+              <input placeholder="caută informații aici"></input>
+              <FaSearch size="18px"></FaSearch>
+            </div>
+            <div className="info-card info-card-news">
+              <div className="card-title">
+                Instalează-ți add-on-ul de depistat știrile false
+              </div>
+            </div>
+            <div className="info-card info-card-news">
+              <div className="card-title">
+                Stiri oficiale si informații la zi
+              </div>
+              <div className="card-content">
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elit,
+                  duis pretium.
+                </p>
+                <button>Cele mai noi informații oficiale</button>
+              </div>
+            </div>
+          </section>
+          <section>
+            <div className="info-card info-card-help">
+              <div className="card-title">Vrei să ajuți. Intră aici</div>
+              <div className="card-content">
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elit,
+                  duis pretium.
+                </p>
+                <button>Centrul de sprijin</button>
+              </div>
+            </div>
+          </section>
+          <section>
+            <div className="info-card info-card-data">
+              <div className="card-title">Date în timp real</div>
+              <div className="card-content">
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elit,
+                  duis pretium.
+                </p>
+                <button>Vezi situația curentă </button>
+              </div>
+            </div>
+          </section>
+        </div>
+      </div>
+    );
+  }
+}
