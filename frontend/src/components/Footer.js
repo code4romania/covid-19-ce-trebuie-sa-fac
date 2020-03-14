cedf3fb8f949763350284b9ee80b6df6ad572b6e
adascaliteiradu <adascalitei.radu@live.com>
2020-03-14 19:01:08 +0200

    Feature/about page (#42)
    * chore: create about page layout
    
    * chore: added useful informations section
    
    * chore: added some mobile friendly css
    
    * chore: added node-sass
    
    * fix: update custom elements to classes, removed ids, fixed formatting, added target to <a> tags
    
    * chore: update app content width
    
    * fix: fixed app content height
    
    * chore: update content width

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Footer = () => {
-  return <footer className="App-footer"></footer>;
+  return <footer className="App-footer"> </footer>;
 };
 
 export default Footer;
