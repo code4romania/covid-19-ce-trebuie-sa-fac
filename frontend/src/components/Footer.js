bfb69da0bea5ca089efa26ad4bcedeffdb6f3348
Dan Lepadatu <lepadatudan@gmail.com>
2020-03-14 11:06:51 +0100

    refactor: change single to double quotes

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,10 +1,7 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
-    return (
-        <footer className="App-footer">
-        </footer>
-    );
+  return <footer className="App-footer"></footer>;
 };
 
 export default Footer;
