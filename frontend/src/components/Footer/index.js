a3b457fd000c5b968af42723aeab6cef055311a6
AndreiMitrea <andrei.mitrea@imbuelabs.com>
2020-03-14 17:46:36 +0200

    Made code review changes

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -1,14 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import code4romania_gray from "../images/code4romania-gray.svg";
-import code4romania_taskforce from "../images/code4romania-taskforce.svg";
+import code4romania_gray from "../../images/code4romania-gray.svg";
+import code4romania_taskforce from "../../images/code4romania-taskforce.svg";
+import "./Footer.scss";
 
 const Footer = () => {
   return (
     <footer className="App-footer">
       <div className="footer-bar">
         <label>proiect incubat în programul</label>
-        <a href="https://code4.ro/ro/">
+        <a
+          href="https://code4.ro/ro/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <img
             src={code4romania_taskforce}
             alt="Code for Romania task force logo"
@@ -27,19 +32,33 @@ const Footer = () => {
             </Link>
           </li>
           <li>
-            <a href="https://code4.ro/ro/codul-de-conduita/">
+            <a
+              href="https://code4.ro/ro/codul-de-conduita/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               Codul de Conduită
             </a>
           </li>
           <li>
-            <a href="https://code4.ro/ro/">Code for Romania</a>
+            <a
+              href="https://code4.ro/ro/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Code for Romania
+            </a>
           </li>
           <li>
             <a href="mailto:taskforce@code4.ro">Contact</a>
           </li>
         </ul>
         <div className="code4-branding">
-          <a href="https://code4.ro/ro/">
+          <a
+            href="https://code4.ro/ro/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <img src={code4romania_gray} alt="Code for Romania gray logo"></img>
           </a>
           <p>© 2020 Code for Romania.</p>
