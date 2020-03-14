a1abf741b9dde6a3b99217ef2fe4895fb5cbfe63
adascaliteiradu <adascalitei.radu@live.com>
2020-03-14 21:24:56 +0200

    chore: added home page, created basic layout for home page, added usefultools component, extract share component for further reuse

--- frontend/src/components/About/index.jsx
+++ frontend/src/components/About/index.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import covid_all from "../../images/covid_all.svg";
 import logo_large from "../../images/logo_large.svg";
-import { FaFacebook, FaLinkedin, FaInstagram, FaTwitter } from "react-icons/fa";
 import "./About.scss";
 import UsefulTools from "../UsefulTools";
+import Share from "../Share";
 
 const About = () => {
   return (
@@ -24,38 +24,7 @@ const About = () => {
             facilisi. Sed risus nullam cursus et dui nunc tincidunt tortor.
           </p>
           <div className="section-footer">
-            <span>Share on</span>
-            <a
-              href="https://www.facebook.com/code4romania/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FaFacebook style={{ color: "#3b5998" }} size="30px"></FaFacebook>
-            </a>
-            <a
-              href="https://www.linkedin.com/company/code4romania/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FaLinkedin style={{ color: "#0e76a8" }} size="30px"></FaLinkedin>
-            </a>
-            <a
-              href="https://www.instagram.com/code4romania/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FaInstagram
-                style={{ color: "#833AB4" }}
-                size="30px"
-              ></FaInstagram>
-            </a>
-            <a
-              href="https://twitter.com/code4romania"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FaTwitter style={{ color: "#42D2F2" }} size="30px"></FaTwitter>
-            </a>
+            <Share></Share>
           </div>
         </section>
         <section className="about-section-logo">
