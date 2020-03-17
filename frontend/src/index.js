11a88733b60446344cd4e6d0787a363cdaf4c094
Utwo <mihai.legat@gmail.com>
2020-03-17 21:52:32 +0200

    Remove css import

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.scss";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import "taskforce-fe-components/dist/index.css";
+// import "taskforce-fe-components/dist/index.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
