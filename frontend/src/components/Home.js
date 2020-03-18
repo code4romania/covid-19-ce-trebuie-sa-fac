2b908d4323bed877ddfecbfaf8b09fe2c1725289
Dan Lepadatu <lepadatudan@gmail.com>
2020-03-18 10:21:18 +0100

    chore: add taskforce-fe-components package (#50)

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { Button } from "@code4ro/taskforce-fe-components";
 
 const Home = () => {
   return (
     <div className="App-content">
       <h1>Acasa</h1>
+      <Button>Test Taskforce Compoent</Button>
     </div>
   );
 };
