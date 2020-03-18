b7d16e1e7bd9cf8208dafe8626c24839d48bac70
anca <anca.cismasiu@gmail.com>
2020-03-18 23:26:26 +0200

    Added header and developed by components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,47 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Header from "./components/Header";
+import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
 import Footer from "./components/Footer";
 import About from "./components/About";
 import Home from "./components/Home";
 import PrivacyPolicy from "./components/PrivacyPolicy";
 import TermsAndConditions from "./components/TermsAndConditions";
+import { Header, DevelopedBy } from "@code4ro/taskforce-fe-components";
+import LogoSvg from "./images/logo_small.svg";
 import "./App.scss";
+
+const Logo = () => (
+  <Link to="/">
+    <img alt="" src={LogoSvg} />
+  </Link>
+);
+
+const MenuItems = [
+  <Link to="/despre" key={"des"}>
+    Despre
+  </Link>,
+  <a
+    href="https://code4.ro/ro/apps/"
+    target="_blank"
+    rel="noopener noreferrer"
+    key={"eco"}
+  >
+    Ecosistemul Covid-19
+  </a>,
+  <a
+    href="https://code4.ro/ro/doneaza/"
+    target="_blank"
+    rel="noopener noreferrer"
+    key={"don"}
+  >
+    Sprijină proiectul
+  </a>
+];
 
 function App() {
   return (
     <Router>
-      <Header />
+      <Header Logo={Logo()} MenuItems={MenuItems} />
+      <DevelopedBy />
 
       <Switch>
         <Route path="/despre">
