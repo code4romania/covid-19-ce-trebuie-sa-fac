38b1715ec9e1bb228c5aaea858efc3fbb1642f36
Radu Adascalitei <adascalitei.radu@live.com>
2020-03-20 00:07:18 +0200

    Feat/decision tree (#46)
    * feat: created decision tree component
    
    * chore: read data via props
    
    * chore: use taskforce components
    
    * chore: use function component
    
    * Update frontend/src/components/DecisionTree/index.jsx
    
    Co-Authored-By: Dan Lepadatu <lepadatudan@gmail.com>
    
    * Update frontend/src/components/DecisionTree/index.jsx
    
    Co-Authored-By: Dan Lepadatu <lepadatudan@gmail.com>
    
    * Update frontend/src/components/Home.js
    
    Co-Authored-By: Dan Lepadatu <lepadatudan@gmail.com>
    
    * Update frontend/src/components/DecisionTree/index.jsx
    
    Co-Authored-By: Dan Lepadatu <lepadatudan@gmail.com>
    
    * Update frontend/src/components/DecisionTree/index.jsx
    
    Co-Authored-By: Dan Lepadatu <lepadatudan@gmail.com>
    
    * Update frontend/src/components/DecisionTree/index.jsx
    
    Co-Authored-By: Dan Lepadatu <lepadatudan@gmail.com>
    
    * chore: removed unused styles
    
    * chore: remove default onClick because it will be implemented in the component
    
    * chore: lint checks
    
    Co-authored-by: Dan Lepadatu <lepadatudan@gmail.com>

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,11 +1,12 @@
 import React from "react";
-import { Button } from "@code4ro/taskforce-fe-components";
+import DecisionTree from "./DecisionTree";
+import Data from "./mock.json";
 
 const Home = () => {
   return (
     <div className="App-content">
       <h1>Acasa</h1>
-      <Button>Test Taskforce Compoent</Button>
+      <DecisionTree data={Data}></DecisionTree>
     </div>
   );
 };
