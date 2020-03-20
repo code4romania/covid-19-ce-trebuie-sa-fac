1635c020aec0fe6abf4ff5608389702033c25645
Irina Borozan <anirib@gmail.com>
2020-03-20 19:18:16 +0200

    added stiri oficiale box data

--- frontend/src/data/useful-apps.js
+++ frontend/src/data/useful-apps.js
@@ -1,57 +1,17 @@
 export default [
   {
-    title: "Date în timp real",
-    doc_type: "USEFUL_APP",
-    doc_id: 4,
-    active: true,
-    display_order: 4,
-    app_type: "DATA",
-    content: "Lorem ipsum dolor sit amet...",
-    buttons: [
-      {
-        title: "Vezi situația curentă",
-        link: "https://test.com"
-      }
-    ]
-  },
-  {
-    title: "Instalează-ți add-on-ul de depistat știrile false",
+    title: "Informații din surse oficiale",
     doc_type: "USEFUL_APP",
     doc_id: 1,
     active: true,
     display_order: 1,
     app_type: "NEWS",
-    content: "Lorem ipsum dolor sit amet...",
-    link: "https://test.com"
-  },
-  {
-    title: "Știri oficiale și informații la zi",
-    doc_type: "USEFUL_APP",
-    doc_id: 2,
-    active: true,
-    display_order: 2,
-    app_type: "NEWS",
-    content: "Lorem ipsum dolor sit amet...",
+    content: null,
     buttons: [
       {
-        title: "Cele mai noi informații oficiale",
-        link: "https://test.com"
+        title: "Știri Oficiale",
+        link: "https://stirioficiale.ro/"
       }
     ]
-  },
-  {
-    title: "Vrei să ajuti. Intră aici",
-    doc_type: "USEFUL_APP",
-    doc_id: 3,
-    active: true,
-    display_order: 3,
-    app_type: "OFFER_HELP",
-    content: "Lorem ipsum dolor sit amet...",
-    buttons: [
-      {
-        title: "Centrul de sprijin",
-        link: "https://test.com"
-      }
-    ]
-  },
+  }
 ];
