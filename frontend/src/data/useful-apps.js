b6687ddf3ca4e537030c0c6a0a1e1d22f5121924
Irina Borozan <anirib@gmail.com>
2020-03-21 13:53:50 +0200

    updated useful apps in sidebar

--- frontend/src/data/useful-apps.js
+++ frontend/src/data/useful-apps.js
@@ -1,6 +1,6 @@
 export default [
   {
-    title: "Știri oficiale și informații la zi",
+    title: "Instalează-ți extensia de Chrome pentru a depista știrile false",
     doc_type: "USEFUL_APP",
     doc_id: 1,
     active: true,
@@ -9,7 +9,39 @@ export default [
     content: null,
     buttons: [
       {
-        title: "Știri Oficiale",
+        title: "Instalează extensie",
+        link: "https://chrome.google.com/webstore/detail/covid-19-stiri-oficiale/pdcpkplohipjhdfdchpmgekifmcdbnha"
+      }
+    ]
+  }
+  ,
+  {
+    title: "Instalează-ți extensia de Firefox pentru a depista știrile false",
+    doc_type: "USEFUL_APP",
+    doc_id: 2,
+    active: true,
+    display_order: 2,
+    app_type: "NEWS",
+    content: null,
+    buttons: [
+      {
+        title: "Instalează extensie",
+        link: "https://addons.mozilla.org/en-US/firefox/addon/covid-19-%C8%99tiri-oficiale/"
+      }
+    ]
+  }
+  ,
+  {
+    title: "Știri oficiale și informații la zi",
+    doc_type: "USEFUL_APP",
+    doc_id: 3,
+    active: true,
+    display_order: 3,
+    app_type: "NEWS",
+    content: null,
+    buttons: [
+      {
+        title: "Cele mai noi informații oficiale",
         link: "https://stirioficiale.ro/"
       }
     ]
