37ad3076c4998f6f618cf184c759a41ba8361955
Irina Borozan <anirib@gmail.com>
2020-03-21 14:18:54 +0200

    added stiri oficiale box data (#82)
    * added stiri oficiale box data
    
    * updates sidebar item title
    
    * updated useful apps in sidebar

--- frontend/src/data/useful-apps.js
+++ frontend/src/data/useful-apps.js
@@ -1,57 +1,49 @@
 export default [
   {
-    title: "Date în timp real",
-    doc_type: "USEFUL_APP",
-    doc_id: 4,
-    active: true,
-    display_order: 4,
-    app_type: "DATA",
-    content: "Lorem ipsum dolor sit amet...",
-    buttons: [
-      {
-        title: "Vezi situația curentă",
-        link: "https://test.com"
-      }
-    ]
-  },
-  {
-    title: "Instalează-ți add-on-ul de depistat știrile false",
+    title: "Instalează-ți extensia de Chrome pentru a depista știrile false",
     doc_type: "USEFUL_APP",
     doc_id: 1,
     active: true,
     display_order: 1,
     app_type: "NEWS",
-    content: "Lorem ipsum dolor sit amet...",
-    link: "https://test.com"
-  },
+    content: null,
+    buttons: [
+      {
+        title: "Instalează extensie",
+        link: "https://chrome.google.com/webstore/detail/covid-19-stiri-oficiale/pdcpkplohipjhdfdchpmgekifmcdbnha"
+      }
+    ]
+  }
+  ,
   {
-    title: "Știri oficiale și informații la zi",
+    title: "Instalează-ți extensia de Firefox pentru a depista știrile false",
     doc_type: "USEFUL_APP",
     doc_id: 2,
     active: true,
     display_order: 2,
     app_type: "NEWS",
-    content: "Lorem ipsum dolor sit amet...",
+    content: null,
     buttons: [
       {
-        title: "Cele mai noi informații oficiale",
-        link: "https://test.com"
+        title: "Instalează extensie",
+        link: "https://addons.mozilla.org/en-US/firefox/addon/covid-19-%C8%99tiri-oficiale/"
       }
     ]
-  },
+  }
+  ,
   {
-    title: "Vrei să ajuti. Intră aici",
+    title: "Știri oficiale și informații la zi",
     doc_type: "USEFUL_APP",
     doc_id: 3,
     active: true,
     display_order: 3,
-    app_type: "OFFER_HELP",
-    content: "Lorem ipsum dolor sit amet...",
+    app_type: "NEWS",
+    content: null,
     buttons: [
       {
-        title: "Centrul de sprijin",
-        link: "https://test.com"
+        title: "Cele mai noi informații oficiale",
+        link: "https://stirioficiale.ro/"
       }
     ]
-  },
+  }
 ];
