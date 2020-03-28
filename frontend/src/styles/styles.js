47d4bac77adc8fe3b46e9d51f9b3febca900423c
Veronica Mihai <veronica.mihai90@outlook.com>
2020-03-28 17:45:04 +0200

    149 mituri despre coronavirus => add additional styles

--- frontend/src/styles/styles.js
+++ frontend/src/styles/styles.js
@@ -1,4 +1,6 @@
 export default {
-  "grey-700-uppercase": "text-transform: uppercase; color: #828282",
-  "no-bullet": "list-style-type:none"
+  "grey-700-uppercase": "text-transform: uppercase; color: #828282;",
+  "no-bullet": "list-style-type:none;",
+  "no-left-margin": "margin-left:0;",
+  "margin-right-10": "margin-right: 10px;"
 };
