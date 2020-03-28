4b4485d2029249dc4b73293de2a621d4e2c93bdc
Veronica Mihai <veronica.mihai90@outlook.com>
2020-03-28 16:20:23 +0200

    lint:fix

--- frontend/src/styles/styles.js
+++ frontend/src/styles/styles.js
@@ -1,4 +1,4 @@
 export default {
-    "grey-700-uppercase": "text-transform: uppercase; color: #828282",
-    "no-bullet": "list-style-type:none"
+  "grey-700-uppercase": "text-transform: uppercase; color: #828282",
+  "no-bullet": "list-style-type:none"
 };
