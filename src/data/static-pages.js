6e27b0e0235642c6224fe4827b27b961ce61f73b
Irina Borozan <anirib@gmail.com>
2020-11-17 10:37:10 +0200

    add one more paragraph (#251)

--- src/data/static-pages.js
+++ src/data/static-pages.js
@@ -43,6 +43,9 @@ export default [
         display_order: 1,
         slug: "/",
         page: `
+	<div style="background-color: #E5F9FE; padding: 10px;">
+	Guvernul României a prelungit starea de alertă pe teritoriul României. Mai jos puteți citi regulile de conduită sanitară aplicate la alegerile locale din septembrie 2020. Regulile pentru alegerile parlamentare vor fi actualizate în curând.
+	</div>
       <p>
         Cetățenii care doresc să participe la alegerile locale din data de 27 septembrie 2020 trebuie să respecte următoarele reguli de protecție sanitară:
         <ul>
