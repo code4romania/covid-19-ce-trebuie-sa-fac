6a991e443ae820203e076b670a509bac8d6792f2
Andrei Ioniță <hi@andrei.io>
2023-06-28 17:27:16 +0100

    wip

--- src/analyticsTracker.js
+++ src/analyticsTracker.js
@@ -1,15 +1,15 @@
 import ReactGA from "react-ga4";
 
 export const initializeGA = () => {
-  if (document.location.hostname !== "cetrebuiesafac.ro") {
-    return;
-  }
+  // if (document.location.hostname !== "cetrebuiesafac.ro") {
+  //   return;
+  // }
   ReactGA.initialize("G-JRHJS1JFC5");
 };
 
 export const logPageView = (history) => {
   history.listen((location) => {
     const page = location.pathname || window.location.pathname;
-    ReactGA.send({ hitType: "pageview", page: page });
+    ReactGA.set({ page: page });
   });
 };
