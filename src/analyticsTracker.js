4248b88a22a6f827df827f994513ecd727585bad
Andrei Ioniță <hi@andrei.io>
2023-06-28 17:31:40 +0100

    Maintenance (#284)
    * disable dependabot updates
    
    * bump node version
    
    * upgrade to react-ga4

--- src/analyticsTracker.js
+++ src/analyticsTracker.js
@@ -1,16 +1,15 @@
-import ReactGA from "react-ga";
+import ReactGA from "react-ga4";
 
 export const initializeGA = () => {
   if (document.location.hostname !== "cetrebuiesafac.ro") {
     return;
   }
-  ReactGA.initialize("UA-160603540-1");
+  ReactGA.initialize("G-JRHJS1JFC5");
 };
 
 export const logPageView = (history) => {
   history.listen((location) => {
     const page = location.pathname || window.location.pathname;
     ReactGA.set({ page: page });
-    ReactGA.pageview(page);
   });
 };
